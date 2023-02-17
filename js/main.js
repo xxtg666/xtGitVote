4e9b102585ce10d0f235b5eec5d43df6c277aaef
xxtg666 <1747433912@qq.com>
2023-02-17 20:34:14 +0800

    github login test

--- js/main.js
+++ js/main.js
@@ -3,16 +3,10 @@ let clientSecret = "4349f2638965852f835084030d98b28250fa3d43"
 function getQueryVariable(variable)
 {
     var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i=0;i<vars.length;i++) {
-        var pair = vars[i].split("=");
-        if(pair[0] == variable){return pair[1];}
-    }
-    return false;
+    return _getQueryVariable(query,variable)
 }
 function _getQueryVariable(query,variable)
 {
-    var query = window.location.search.substring(1);
     var vars = query.split("&");
     for (var i=0;i<vars.length;i++) {
         var pair = vars[i].split("=");
